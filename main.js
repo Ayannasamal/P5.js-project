14a19c0173e0b7534ba98cc6bbdf9032925e7c2e
Ayannasamal <96683706+Ayannasamal@users.noreply.github.com>
2023-05-08 11:10:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+function preload() {
+
+}
+
+function setup() {
+canvas = createCanvas(640 , 480);
+canvas.position(110 , 250);
+video = createCapture(VIDEO);
+video.hide();
+
+}
+
+function draw() {
+image(video , 230 , 150 , 220 , 200)
+fill("green")
+rect(90,40,460,20)
+rect(90,420,460,20)
+rect(40,90,20,300)
+rect(580,90,20,300)
+fill("red")
+circle(50,50,80)
+circle(50,430,80)
+circle(590,50,80)
+circle(590,430,80)
+}
+
+function filter_function() {
+    tint_colour=document.getElementById("Colour").value ;
+}
+
+function take_snapshot() {
+    save("filter.png")
+}
